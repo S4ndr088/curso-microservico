e74a5be61d5949c2a801a27092d6cb6ae5e210ba
Sandro <sandrorv88@gmail.com>
2026-06-09 22:24:22 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-service/src/main/java/com/curso/hreurekaservice/HrEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.curso.hreurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServiceApplication.class, args);
+	}
+
+}
