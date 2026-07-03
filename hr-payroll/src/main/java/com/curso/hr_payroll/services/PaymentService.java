7cc459983e0c24705c86be99be427324ff06b937
Sandro <sandrorv88@gmail.com>
2026-07-03 05:38:11 -0300

    Random port to hr-work.

--- hr-payroll/src/main/java/com/curso/hr_payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/curso/hr_payroll/services/PaymentService.java
@@ -4,7 +4,6 @@ import com.curso.hr_payroll.entities.Payment;
 import com.curso.hr_payroll.entities.Worker;
 import com.curso.hr_payroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
